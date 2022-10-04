a2f1dbd301bd516a7b3768cf439cd99007e23145
estuhub <107235863+estuhub@users.noreply.github.com>
2022-10-04 19:54:36 +0700

    create location structure

---
+++ src/Components/location.js
@@ -0,0 +1,155 @@
+// import
+import React from "react";
+
+// create classes
+class Location extends React.Component {
+  render() {
+    return (
+      <>
+        <div class="container">
+          {/* location */}
+          <h2 class="pt-4">Koh Phangan</h2>
+          <small class="card-text">120 places</small>
+          <div class="row">
+            {/* card section */}
+            <div class="col">
+              <div class="wrap"></div>
+            </div>
+          </div>
+          {/* end of location section*/}
+          {/* activities & map section */}
+          <div class="row mt-4">
+            <div class="col-6">
+              <h5 class="mb-3">Things to do</h5>
+              {/* category start */}
+              <h6>Sunsets</h6>
+              {/* accordion start */}
+              <div class="accordion accordion-flush" id="accordionExample">
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="headingOne">
+                    <button
+                      class="accordion-button"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseOne"
+                      aria-expanded="true"
+                      aria-controls="collapseOne"
+                    >
+                      Zen Beach with Drums
+                    </button>
+                  </h2>
+                  <div
+                    id="collapseOne"
+                    class="accordion-collapse collapse show"
+                    aria-labelledby="headingOne"
+                    data-bs-parent="#accordionExample"
+                  >
+                    <div class="accordion-body">
+                      <div class="row">
+                        <div class="col-6">
+                          <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua. Ut enim ad minim veniam, quis
+                            nostrud exercitation ullamco laboris nisi ut aliquip
+                            ex ea commodo consequat.
+                          </p>
+                          <span>Adress: Zen Beach, Koh Phangan</span>
+                        </div>
+                        <div class="col-6">
+                          <img
+                            src="https://www.asiablue-scuba.com/wp-content/uploads/2021/12/zen-beach-koh-phangan.jpg"
+                            class="img-fluid"
+                            alt=""
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="headingTwo">
+                    <button
+                      class="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseTwo"
+                      aria-expanded="false"
+                      aria-controls="collapseTwo"
+                    >
+                      Cocohut Resort
+                    </button>
+                  </h2>
+                  <div
+                    id="collapseTwo"
+                    class="accordion-collapse collapse"
+                    aria-labelledby="headingTwo"
+                    data-bs-parent="#accordionExample"
+                  >
+                    <div class="accordion-body">
+                      <strong>This is the second item's accordion body.</strong>{" "}
+                      It is hidden by default, until the collapse plugin adds
+                      the appropriate classes that we use to style each element.
+                      These classes control the overall appearance, as well as
+                      the showing and hiding via CSS transitions. You can modify
+                      any of this with custom CSS or overriding our default
+                      variables. It's also worth noting that just about any HTML
+                      can go within the
+                      <code>.accordion-body</code>, though the transition does
+                      limit overflow.
+                    </div>
+                  </div>
+                </div>
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="headingThree">
+                    <button
+                      class="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseThree"
+                      aria-expanded="false"
+                      aria-controls="collapseThree"
+                    >
+                      Apachada View Point
+                    </button>
+                  </h2>
+                  <div
+                    id="collapseThree"
+                    class="accordion-collapse collapse"
+                    aria-labelledby="headingThree"
+                    data-bs-parent="#accordionExample"
+                  >
+                    <div class="accordion-body">
+                      <strong>This is the third item's accordion body.</strong>{" "}
+                      It is hidden by default, until the collapse plugin adds
+                      the appropriate classes that we use to style each element.
+                      These classes control the overall appearance, as well as
+                      the showing and hiding via CSS transitions. You can modify
+                      any of this with custom CSS or overriding our default
+                      variables. It's also worth noting that just about any HTML
+                      can go within the
+                      <code>.accordion-body</code>, though the transition does
+                      limit overflow.
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* accordion end */}
+            </div>
+            <div class="col-6">
+              {/* map */}
+              <img
+                src="https://phanganlandandhome.com/wp-content/uploads/2020/01/Koh-Phangan-Map.png"
+                class="img-fluid"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+// export
+export default Location;
