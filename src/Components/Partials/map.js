d8f53511a3704f3e42b32dde60ba9e50b3be7fb4
indeva-git <in-deva@hotmail.com>
2022-10-06 14:29:05 +0700

    Set up marker on click action

--- src/Components/Partials/map.js
+++ src/Components/Partials/map.js
@@ -6,7 +6,10 @@ import GoogleMap from "google-map-react"
 
 const Marker = props => {
   return (
-		<div className="SuperAwesomePin">
+		<div className="SuperAwesomePin"
+		onClick={(e) => {	console.log(`marker coordinates`)
+			console.log('ah')}}
+		>
 			<img
 				src="https://th.bing.com/th/id/OIP.PswRYJEO6ptKds__PAoKXQAAAA?pid=ImgDet&w=300&h=275&rs=1"
 				alt=""
@@ -29,6 +32,19 @@ class Map extends React.Component {
       zoom: 12
     }
   }
+	mapOnClick = (e) => {
+		console.log('click coordinates')
+		this.logLatLong(e)
+	}
+	logLatLong = (e) => {
+		console.log("latitude = ", e.lat)
+		console.log("longtitude = ", e.lng)
+	}
+	markerOnClick = (marker) => {
+		console.log(`marker coordinates`)
+		console.log(marker.geometry.location.lat)
+		console.log(marker.geometry.location.lng)
+	}
   render() {
 		const styles = {
     map: {
@@ -46,8 +62,14 @@ class Map extends React.Component {
           bootstrapURLKeys={this.state.map.key}
           center={this.state.map.center}
           zoom={this.state.map.zoom}
+					onClick={(e) => {this.mapOnClick(e)}}
         >
-				<Marker lat={9.730213} lng={100.017587} />
+					<Marker
+					lat={9.730213}
+					lng={100.017587}
+					onClick={(e) => {this.markerOnClick(e)}}
+
+					/>
 				</GoogleMap>
       </div>
     )
