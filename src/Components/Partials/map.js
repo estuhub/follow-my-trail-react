932f4ff06f5ff01109a14380bd5569601e3b5b87
indeva-git <in-deva@hotmail.com>
2022-10-06 14:13:46 +0700

    Test map marker setup and working

--- src/Components/Partials/map.js
+++ src/Components/Partials/map.js
@@ -2,6 +2,19 @@
 import React from "react"
 // import axios from 'axios'
 import GoogleMap from "google-map-react"
+// import Marker from './marker.js'
+
+const Marker = props => {
+  return (
+		<div className="SuperAwesomePin">
+			<img
+				src="https://th.bing.com/th/id/OIP.PswRYJEO6ptKds__PAoKXQAAAA?pid=ImgDet&w=300&h=275&rs=1"
+				alt=""
+				class="avatar-img"
+			/>
+	</div>
+	)
+}
 
 class Map extends React.Component {
   state = {
@@ -21,7 +34,11 @@ class Map extends React.Component {
     map: {
 			width: '600px',
   		height: '600px'
-    }
+    },
+		pin: {
+			width: '50px',
+			height: '50px'
+		}
 	}
     return (
       <div className="map" style={styles.map}>
@@ -30,11 +47,14 @@ class Map extends React.Component {
           center={this.state.map.center}
           zoom={this.state.map.zoom}
         >
+				<Marker lat={9.730213} lng={100.017587} />
 				</GoogleMap>
       </div>
     )
   }
 }
+
+
 
 // export
 export default Map
