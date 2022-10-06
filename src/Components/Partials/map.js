80c688f089b8f9680713665554418f9d9e97b032
indeva-git <in-deva@hotmail.com>
2022-10-06 16:32:36 +0700

    Map loads geometry from backend; api key from .env file; .env-sample created

--- src/Components/Partials/map.js
+++ src/Components/Partials/map.js
@@ -1,6 +1,6 @@
 // import
 import React from "react"
-// import axios from 'axios'
+import axios from 'axios'
 import GoogleMap from "google-map-react"
 import Marker from './marker.js'
 
@@ -9,16 +9,30 @@ import Marker from './marker.js'
 class Map extends React.Component {
   state = {
     map: {
-      key: {
-        key: "AIzaSyBA6mFqPpHyp657OgfA-dQio8hVTGVgOUk"
-      },
       center: {
-        lat: 9.730213,
-        lng: 100.017587
+        lat: 55,
+        lng: 0
       },
-      zoom: 12
+      zoom: 5
     }
   }
+	setMap = async () => {
+		let mapGeometry = await axios.get(`${process.env.REACT_APP_SERVER_URL}/map`)
+		mapGeometry = JSON.parse(mapGeometry.request.response)
+		this.setState ({
+			map: {
+				center: {
+					lat: mapGeometry.geometry.lat,
+					lng: mapGeometry.geometry.lng,
+				},
+				zoom: mapGeometry.geometry.zoom
+				}
+		})
+	}
+	componentWillMount() {
+		this.setMap()
+	}
+
 	mapOnClick = (e) => {
 		console.log('click coordinates')
 		this.logLatLong(e)
@@ -32,6 +46,7 @@ class Map extends React.Component {
 		console.log(marker.geometry.location.lat)
 		console.log(marker.geometry.location.lng)
 	}
+
   render() {
 		const styles = {
     map: {
@@ -46,7 +61,7 @@ class Map extends React.Component {
     return (
       <div className="map" style={styles.map}>
         <GoogleMap
-          bootstrapURLKeys={this.state.map.key}
+          bootstrapURLKeys={{key: process.env.REACT_APP_GOOGLE_API_KEY}}
           center={this.state.map.center}
           zoom={this.state.map.zoom}
 					onClick={(e) => {this.mapOnClick(e)}}
