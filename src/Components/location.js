9ccdbff565d2b2535bc358a029b372b82a903bad
indeva-git <in-deva@hotmail.com>
2022-10-08 11:37:05 +0700

    Fixed location page layout

--- src/Components/location.js
+++ src/Components/location.js
@@ -72,75 +72,66 @@ class Location extends React.Component {
   render() {
     return (
 			<>
-        <div class="container">
-          {/* location */}
-          <h2 class="pt-4">Koh Phangan</h2>
-          <small class="card-text">120 places</small>
-          <div class="row">
-            {/* card section */}
-            <div class="col">
-              <div class="wrap"></div>
-            </div>
-          </div>
-          {/* end of location section*/}
-          {/* activities & map section */}
-          <div class="row mt-4">
-            <div class="col-6">
-              <h5 class="mb-3">Things to do</h5>
-              {/* category start */}
-              <h6>Sunsets</h6>
-							{/* accordion start */}
-	              <div class="accordion accordion-flush" id="accordionExample">
-									{this.state.places.map((place, i) => (
-										<div class="accordion-item">
-		                  <h2 class="accordion-header" id={`heading${i}`}>
-		                    <button
-		                      class="accordion-button collapsed"
-		                      type="button"
-		                      data-bs-toggle="collapse"
-		                      data-bs-target={`#collapse${i}`}
-		                      aria-expanded="false"
-		                      aria-controls={`collapse${i}`}
-													onClick={ev => this.zoomMap(ev.target.innerHTML, place.geometry)}
-		                    >
-												{place.name}
-		                    </button>
-		                  </h2>
-		                  <div
-		                    id={`collapse${i}`}
-												className={place.name === this.state.focus ? "accordion-collapse collapse show" : "accordion-collapse collapse collapsed"}
-		                    aria-labelledby={`heading{i}`}
-		                    data-bs-parent="#accordionExample"
-		                  >
-		                    <div class="accordion-body">
-		                      <div class="row">
-		                        <div class="col-6">
-		                          <p>
-		                            {place.description}
-		                          </p>
-															<p>{place.address}</p>
-		                        </div>
-		                        <div class="col-6">
-		                          <img
-		                            src={place.images[0]}
-		                            class="img-fluid"
-		                            alt=""
-		                          />
-		                        </div>
-		                      </div>
-		                    </div>
-		                  </div>
-		                </div>
-									))}
-								</ div>
-              {/* accordion end */}
-            	<div class="col-6">
-              	{/* map */}
-              <Map map={this.state.map} updateFocus={this.updateFocus}/>
-            </div>
-          </div>
-        </div>
-			</div>
+			  <div class="container">
+					{/* location */}
+					<h2 class="pt-4">Koh Phangan</h2>
+					<small class="card-text">120 places</small>
+					{/* end of location section*/}
+					<div class="row">
+						<div class="col">
+						<h5 class="mb-3">Things to do</h5>
+						{/* category start */}
+						<h6>Sunsets</h6>
+						{/* accordion start */}
+							<div class="accordion accordion-flush" id="accordionExample">
+								{this.state.places.map((place, i) => (
+									<div class="accordion-item">
+										<h2 class="accordion-header" id={`heading${i}`}>
+											<button
+												class="accordion-button collapsed"
+												type="button"
+												data-bs-toggle="collapse"
+												data-bs-target={`#collapse${i}`}
+												aria-expanded="false"
+												aria-controls={`collapse${i}`}
+												onClick={ev => this.zoomMap(ev.target.innerHTML, place.geometry)}
+											>
+											{place.name}
+											</button>
+										</h2>
+										<div
+											id={`collapse${i}`}
+											className={place.name === this.state.focus ? "accordion-collapse collapse show" : "accordion-collapse collapse collapsed"}
+											aria-labelledby={`heading{i}`}
+											data-bs-parent="#accordionExample"
+										>
+											<div class="accordion-body">
+												<div class="row">
+													<div class="col-6">
+														<p>
+															{place.description}
+														</p>
+														<p>{place.address}</p>
+													</div>
+													<div class="col-6">
+														<img
+															src={place.images[0]}
+															class="img-fluid"
+															alt=""
+														/>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								))}
+							</ div>
+						</div>
+						<div class="col">
+							<Map map={this.state.map} updateFocus={this.updateFocus}/>
+						</div>
+					</div>
+				</div>
 		</>
     );
   }
