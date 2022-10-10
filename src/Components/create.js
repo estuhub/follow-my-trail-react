0de9e6bc9c38fe66e123a3cdf4ba198507e4c26d
indeva-git <in-deva@hotmail.com>
2022-10-10 15:39:03 +0700

    Create form setup to create a document and redirect to location page (no search autocomplete)

--- src/Components/create.js
+++ src/Components/create.js
@@ -2,9 +2,13 @@
 import React from "react";
 import AutoComplete from "./Partials/autocomplete";
 import axios from "axios";
+import { Redirect } from 'react-router-dom'
 
 // create classes
 class Create extends React.Component {
+	state = {
+		activityCreated: false
+	}
   createActivity = async form => {
     form.preventDefault()
     await axios.post(
@@ -15,92 +19,96 @@ class Create extends React.Component {
 				image: form.target.image.value
 			}
     )
-    // this.setState({
-    //   loggedIn: login.data
-    // })
+    this.setState({
+      activityCreated: true //login.data
+    })
   }
   render() {
     return (
       <>
-        <div className="container pt-4">
-          <form onSubmit={ev => this.createActivity(ev)}>
-            <div>
-              <h3>Create new activity</h3>
-              {/* choose category */}
-              <div className="mb-3">
-                <label className="form-label m-0">Category</label>
-                <select name="category" className="form-control" required>
-                  <option>- Choose a category -</option>
-                  <option>Beach</option>
-                  <option>Food</option>
-                  <option>Gems</option>
-                  <option>Hikes</option>
-                  <option>Sunsets</option>
-                  <option>Uncategorized</option>
-                </select>
-              </div>
-              {/* title */}
-              <div className="mb-3">
-                <label className="form-label m-0">Short Title</label>
-                <input
-                  name="title"
-                  type="text"
-                  className="form-control"
-									// eslint-disable-next-line
-                  name="title"
-                  required
-                />
-              </div>
-              {/* description */}
-              <div className="mb-3">
-                <label className="form-label m-0">Description</label>
-                <textarea
-                  name="description"
-                  className="form-control"
-                  rows="7"
-									// eslint-disable-next-line
-                  name="description"
-                  required
-                ></textarea>
-              </div>
-              {/* city */}
-              <div className="mb-3">
-                <label for="exampleFormControlInput1" className="form-label">
-                  City
-                </label>
-                <input type="text" className="form-control" name="city" required />
-              </div>
-              {/* address */}
-              <div className="mb-3">
-                <label
-                  for="exampleFormControlInput1"
-                  className="form-label"
-                  name="address"
-                >
-                  Address
-                </label>
-              </div>
-              {/* picture */}
-              <div className="mb-3">
-                <label for="formFileSm" className="form-label">
-                  Add a picture
-                </label>
-                <input
-                  className="form-control form-control-sm"
-                  id="formFileSm"
-                  type="file"
-                  name="image"
-                />
-              </div>
-              {/* button */}
-              <div>
-                <button className="btn btn-success">Submit</button>
-              </div>
-            </div>
-          </form>
-          {/* like, share, add to my list */}
-          {/* id of creator and id of current user */}
-        </div>
+				{!this.state.activityCreated ?
+	        <div className="container pt-4">
+	          <form onSubmit={ev => this.createActivity(ev)}>
+	            <div>
+	              <h3>Create new activity</h3>
+	              {/* choose category */}
+	              <div className="mb-3">
+	                <label className="form-label m-0">Category</label>
+	                <select name="category" className="form-control" required>
+	                  <option>- Choose a category -</option>
+	                  <option>Beach</option>
+	                  <option>Food</option>
+	                  <option>Gems</option>
+	                  <option>Hikes</option>
+	                  <option>Sunsets</option>
+	                  <option>Uncategorized</option>
+	                </select>
+	              </div>
+	              {/* title */}
+	              <div className="mb-3">
+	                <label className="form-label m-0">Short Title</label>
+	                <input
+	                  name="title"
+	                  type="text"
+	                  className="form-control"
+										// eslint-disable-next-line
+	                  name="title"
+	                  required
+	                />
+	              </div>
+	              {/* description */}
+	              <div className="mb-3">
+	                <label className="form-label m-0">Description</label>
+	                <textarea
+	                  name="description"
+	                  className="form-control"
+	                  rows="7"
+										// eslint-disable-next-line
+	                  name="description"
+	                  required
+	                ></textarea>
+	              </div>
+	              {/* city */}
+	              <div className="mb-3">
+	                <label for="exampleFormControlInput1" className="form-label">
+	                  City
+	                </label>
+	                <input type="text" className="form-control" name="city" required />
+	              </div>
+	              {/* address */}
+	              <div className="mb-3">
+	                <label
+	                  for="exampleFormControlInput1"
+	                  className="form-label"
+	                  name="address"
+	                >
+	                  Address
+	                </label>
+	              </div>
+	              {/* picture */}
+	              <div className="mb-3">
+	                <label for="formFileSm" className="form-label">
+	                  Add a picture
+	                </label>
+	                <input
+	                  className="form-control form-control-sm"
+	                  id="formFileSm"
+	                  type="file"
+	                  name="image"
+	                />
+	              </div>
+	              {/* button */}
+	              <div>
+	                <button className="btn btn-success">Submit</button>
+	              </div>
+	            </div>
+	          </form>
+	          {/* like, share, add to my list */}
+	          {/* id of creator and id of current user */}
+	        </div>
+					:
+					<Redirect to='/location' />
+				}
       </>
     );
   }
